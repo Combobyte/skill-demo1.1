9573a8fe1e36ebd5fcaac78739fa5d9d1a395f64
Combobyte <varun.sreedhar1@gmail.com>
2022-05-01 22:15:08 -0700

    added addTest

--- AdditionTest.java
+++ AdditionTest.java
@@ -1,7 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.*;
 import org.junit.Assert.*;
 
 public class AdditionTest{
 
-
+    @Test
+    public void addTest(){
+        assertEquals("Add test", 3, Addition.add(1, 2));
+    }
 }
